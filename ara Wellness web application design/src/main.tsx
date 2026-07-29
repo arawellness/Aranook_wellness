470d0d5d360a25f326bb21b8692d66d090a11a70
Ara <aishwaryareddykothakapu@gmail.com>
2026-07-29 20:30:19 +0530

    Update main.tsx

--- ara Wellness web application design/src/main.tsx
+++ ara Wellness web application design/src/main.tsx
@@ -1,7 +1,13 @@
+import { createRoot } from "react-dom/client";
+import App from "./app/App.tsx";
+import AnalyticsProvider from "./app/components/AnalyticsProvider";
+import CookieConsent from "./app/components/CookieConsent";
+import "./styles/index.css";
 
-  import { createRoot } from "react-dom/client";
-  import App from "./app/App.tsx";
-  import "./styles/index.css";
-
-  createRoot(document.getElementById("root")!).render(<App />);
-  
+createRoot(document.getElementById("root")!).render(
+  <>
+    <AnalyticsProvider />
+    <App />
+    <CookieConsent />
+  </>
+);
